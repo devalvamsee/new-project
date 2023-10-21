efc6b3f17102e2f5f2c9cd756f26b6758c130c2b
devalvamsee <136257298+devalvamsee@users.noreply.github.com>
2023-10-21 23:04:16 +0530

    Update remove.py

--- remove.py
+++ remove.py
@@ -1,5 +1,12 @@
 import boto3
 
+def get_instance_ids(name_prefix):
+    ec2 = boto3.resource('ec2', region_name='ap-south-1')
+    instances = ec2.instances.filter(
+        Filters=[{'Name': 'tag:Name', 'Values': [f'{name_prefix}*']}]
+    )
+    return [instance.instance_id for instance in instances]
+
 def delete_instances(instance_ids):
     ec2 = boto3.client('ec2', region_name='ap-south-1')
     response = ec2.terminate_instances(InstanceIds=instance_ids)
@@ -15,13 +22,14 @@ def remove_instance_data(instance_ids):
                 'InstanceId': instance_id
             }
         )
-        # Optionally check response for errors
-        # print(response)
 
 try:
-    # Replace with the actual instance IDs you want to delete
-    instance_ids_to_delete = ['i-0abcd1234efgh5678', 'i-0abcd1234efgh5679']
-    deleted_instances = delete_instances(instance_ids_to_delete)
-    remove_instance_data(instance_ids_to_delete)
+    name_prefix = 'app-instance'  # Replace with your actual name prefix
+    instance_ids_to_delete = get_instance_ids(name_prefix)
+    if instance_ids_to_delete:
+        deleted_instances = delete_instances(instance_ids_to_delete)
+        remove_instance_data(instance_ids_to_delete)
+    else:
+        print(f"No instances found with name prefix: {name_prefix}")
 except Exception as e:
     print(f"An error occurred: {e}")
