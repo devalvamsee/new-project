2150c66b2423ab3515a008036a393e3c8b805145
devalvamsee <136257298+devalvamsee@users.noreply.github.com>
2023-10-21 22:28:22 +0530

    Update creation.py

--- creation.py
+++ creation.py
@@ -1,29 +1,27 @@
 import boto3
 
 def create_instance(name_prefix, instance_type, count):
-    ec2 = boto3.client('ec2')
+    ec2 = boto3.client('ec2', region_name='ap-south-1')  # Updated region
+
+    tags_list = [{'Key': 'Name', 'Value': f'{name_prefix}-{i}'} for i in range(1, count + 1)]
     instances = ec2.run_instances(
-        ImageId='ami-12345678',  # Replace with your AMI ID
+        ImageId='ami-0287a05f0ef0e9d9a',  # Updated AMI ID
         InstanceType=instance_type,
         MinCount=count,
         MaxCount=count,
         TagSpecifications=[
             {
                 'ResourceType': 'instance',
-                'Tags': [
-                    {
-                        'Key': 'Name',
-                        'Value': f'{name_prefix}-{i}'
-                    } for i in range(1, count + 1)
-                ]
+                'Tags': tags_list
             },
         ]
     )
     return instances['Instances']
 
 def store_instance_data(instances):
-    dynamodb = boto3.resource('dynamodb')
+    dynamodb = boto3.resource('dynamodb', region_name='ap-south-1')  # Updated region
     table = dynamodb.Table('InstanceInfo')
+
     for instance in instances:
         table.put_item(
             Item={
@@ -34,5 +32,8 @@ def store_instance_data(instances):
             }
         )
 
-instances = create_instance('app-instance', 't2.micro', 5)
-store_instance_data(instances)
+try:
+    instances = create_instance('app-instance', 't2.micro', 5)
+    store_instance_data(instances)
+except Exception as e:
+    print(f"An error occurred: {e}")
